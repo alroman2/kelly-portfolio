222428f9243b1159a032449a77ca53b444bc0ca1
Alex Roman <aroman27@ucmerced.edu>
2023-03-17 20:37:10 -0700

    Created card component

--- src/Components/Carousel/Card.js
+++ src/Components/Carousel/Card.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-const Card = ({image, rotate}) => {
+const Card = ({image, rotateClass, index}) => {
     
+    const style = 'w-[40vw] md:w-[30vw] h-[80vh] lg:w-[20vw] md:h-[50vh] object-contain transform-gpu absolute';
     return(
     <>
-        <div className="w-[20vw] h-[50vh] object-contain rotate-[]">
-            <img src={image} alt="card" />
+        <div className={style} style={{
+            rotate: rotateClass,
+            zIndex: index
+        }}>
+            <img src={image} alt="card" className="drop-shadow-lg rounded-lg" >
+            </img>
         </div>
     </>
     );
