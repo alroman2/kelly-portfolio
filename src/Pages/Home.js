070dd84bf93a36126a91f8376f746a814de81ef4
Alex Roman <aroman27@ucmerced.edu>
2023-03-17 23:34:24 -0700

    Removed old navigation bar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "../Components/Navigation/NavBar";
+
 import Carousel from "../Components/Carousel/Carousel";
 import Navigation from "../Components/Navigation/Navigation";
 const Home = () => {
