a0ce5c823920303f9581a2693564747ddf61eead
Alex Roman <aroman27@ucmerced.edu>
2023-03-18 01:25:36 -0700

    Added nav and footer

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,13 +2,16 @@ import React from "react";
 
 import Carousel from "../Components/Carousel/Carousel";
 import Navigation from "../Components/Navigation/Navigation";
+import Footer from "../Components/Footer/Footer";
 const Home = () => {
 
     return (
         <>  
+           
             <Navigation />
-         
             <Carousel />
+            <Footer/>
+
         </>
     );
 };
